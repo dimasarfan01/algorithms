3e134606236aa35f1d6e8a9f7ceaf6f47e53a912
dimasarfan01 <77246142+dimasarfan01@users.noreply.github.com>
2023-08-14 23:55:04 +0700

    finish even fibonacci numbers

---
+++ free-code-camp/project-euler/largest-prime-factor.ts
@@ -0,0 +1,11 @@
+/*
+    The prime factors of 13195 are 5, 7, 13 and 29.
+
+    What is the largest prime factor of the given number?
+*/
+
+function largestPrimeFactor(n: number) {
+  return true;
+}
+
+console.log(largestPrimeFactor(13195));
